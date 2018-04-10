4a8ba86f7a4e2e481ae87b502645463ee8a43fc9
alichtman <aaronlichtman@gmail.com>
2018-04-10 02:05:32 -0500

    Refactored backup utils and added config parsing

--- shallow-backup.py
+++ shallow-backup.py
@@ -4,7 +4,9 @@
 import os
 import sys
 import click
+import shutil
 import inquirer
+import configparser
 import subprocess as sp
 from colorama import Fore, Style
 from constants import Constants
@@ -13,15 +15,25 @@ from constants import Constants
 def splash_screen():
 
 	print(Fore.YELLOW + Style.BRIGHT + "\n" +
-"            dP                dP dP                              dP                         dP                         \n" +
-"            88                88 88                              88                         88                         \n" +
-"   ,d8888'  88d888b. .d8888b. 88 88 .d8888b. dP  dP  dP          88d888b. .d8888b. .d8888b. 88  .dP  dP    dP 88d888b. \n" +
-"   Y8ooooo, 88'  `88 88'  `88 88 88 88'  `88 88  88  88   8888   88'  `88 88'  `88 88'  `\"\" 88888\"   88    88 88'  `88 \n" +
-"         88 88    88 88.  .88 88 88 88.  .88 88.88b.88'          88.  .88 88.  .88 88.  ... 88  `8b. 88.  .88 88.  .88 \n" +
-"   `88888P' dP    dP `88888P8 dP dP `88888P' 8888P Y8P           88Y8888' `88888P8 `88888P' dP   `YP `88888P' 88Y888P' \n" +
-"                                                                                                              88		\n" +
-"                                                                                                              dP		\n" + Style.RESET_ALL)
+"            dP                dP dP                         dP                         dP                         \n" +
+"            88                88 88                         88                         88                         \n" +
+"   ,d8888'  88d888b. .d8888b. 88 88 .d8888b. dP  dP  dP     88d888b. .d8888b. .d8888b. 88  .dP  dP    dP 88d888b. \n" +
+"   Y8ooooo, 88'  `88 88'  `88 88 88 88'  `88 88  88  88     88'  `88 88'  `88 88'  `\"\" 88888\"   88    88 88'  `88 \n" +
+"         88 88    88 88.  .88 88 88 88.  .88 88.88b.88'     88.  .88 88.  .88 88.  ... 88  `8b. 88.  .88 88.  .88 \n" +
+"   `88888P' dP    dP `88888P8 dP dP `88888P' 8888P Y8P      88Y8888' `88888P8 `88888P' dP   `YP `88888P' 88Y888P' \n" +
+"                                                                                                         88		\n" +
+"                                                                                                         dP		\n" + Style.RESET_ALL)
 
+def overwrite_make_dir(path):
+	"""Make destination dir if it doesn't exist. Overwrite if it does."""
+
+	if not os.path.exists(path):
+		print(Fore.GREEN + path, "was created." + Style.RESET_ALL)
+		os.makedirs(path)
+	else:
+		shutil.rmtree(path)
+		os.makedirs(path)
+		print(Fore.RED + path, "was removed and an updated directory was created." + Style.RESET_ALL)
 
 def backup_prompt():
 	"""Use pick library to prompt user with choice of what to backup."""
@@ -36,17 +48,17 @@ def backup_prompt():
 	return answers.get('choice').strip().lower()
 
 
-def backup_dotfiles():
+def backup_dotfiles(path):
 	"""Creates `dotfiles` directory and places copies of dotfiles there."""
 
-	sp.run("mkdir text_backup/dotfiles", shell=True, stdout=sp.PIPE)
+	overwrite_make_dir(path)
 
 	source_dest = [
-		"/.pypirc text_backup/dotfiles/pypirc.txt",
-		"/.zshrc text_backup/dotfiles/zshrc.txt",
-		"/.bashrc text_backup/dotfiles/bashrc.txt",
-		"/.ssh text_backup/dotfiles/ssh",
-		"/.vim text_backup/dotfiles/vim"
+		"/.pypirc {}/pypirc.txt".format(path),
+		"/.zshrc {}/zshrc.txt".format(path),
+		"/.bashrc {}/bashrc.txt".format(path),
+		"/.ssh {}/ssh".format(path),
+		"/.vim {}/vim".format(path)
 	]
 
 	home_path = os.path.expanduser('~')
@@ -63,9 +75,10 @@ def backup_dotfiles():
 			sp.run(command, shell=True, stdout=sp.PIPE)
 
 
-def backup_installs():
+def backup_installs(path):
 	"""Creates `installs` directory and places install list text files there."""
-	sp.run("mkdir text_backup/installs", shell=True, stdout=sp.PIPE)
+
+	overwrite_make_dir(path)
 
 	command_list = [
 		"brew",
@@ -76,12 +89,19 @@ def backup_installs():
 	]
 
 	for x in command_list:
-		command = x + " list > text_backup/installs/" + x.replace(" ", "_") + "_list.txt"
+		# replace takes care of the space in "brew cask"
+		command = x + " list > {}/".format(path) + x.replace(" ", "_") + "_list.txt"
 		print("CMD", command)
 		sp.run(command, shell=True, stdout=sp.PIPE)
 
 	# special case for system installs
-	sp.run("ls /Applications/ > text_backup/installs/applications_list.txt", shell=True, stdout=sp.PIPE)
+	sp.run("ls /Applications/ > {}/applications_list.txt".format(path), shell=True, stdout=sp.PIPE)
+
+
+def backup_all(installs_path, dotfiles_path):
+	"""Complete backup"""
+	backup_dotfiles(dotfiles_path)
+	backup_installs(installs_path)
 
 
 ######
@@ -106,38 +126,66 @@ def cli(complete, dotfiles, installs, v):
 
 	splash_screen()
 
-	# create text_backup dir
-	if not os.path.exists("text_backup"):
-		sp.run("mkdir text_backup", shell=True, stdout=sp.PIPE)
-	else:
-		print(Fore.RED + Style.BRIGHT + "ERROR: directory `text_backup` already exists in this folder.")
-		sys.exit()
+	# Read config file
+	config = configparser.ConfigParser()
+	config.read("config.ini")
+	path = config['Paths']['backup_dir']
+	# print("Backup Dir", path)
 
-	selection = ""
+	# if path is "", that means that user has never run before.
+	if path == "":
+		# Make sure folder with that path doesn't exist already
+		if not os.path.exists("shallow_backup"):
+			print(Fore.GREEN + Style.BRIGHT + "Creating default shallow_backup directory in this folder.")
+			overwrite_make_dir("shallow_backup")
+
+			# get absolute path, write it to config file, set for later in program
+			path = os.path.abspath("./shallow_backup")
+			config['Paths']['backup_dir'] = path
+
+			with open('config.ini', 'w') as configfile:
+				config.write(configfile)
+
+			print("Abs path:", path)
+
+		# Edge case: User has shallow_backup dir that was not created by this program (or by an older version of this program)
+		else:
+			print(Fore.GREEN + Style.BRIGHT + "ERROR: shallow_backup directory already in this folder.")
+			sys.exit()
+
+	# path is not "". Use absolute path in config file.
+	else:
+		print(Fore.GREEN + Style.BRIGHT + "Reading config file to get `shallow_backup` directory path.")
+
+	dotfiles_path = os.path.join(path, "dotfiles")
+	installs_path = os.path.join(path, "installs")
+
+	print("Dots:", dotfiles_path)
+	print("Installs:", installs_path)
+
+	# Command line options
+	if complete or dotfiles or installs:
+		if complete:
+			backup_all(dotfiles_path, installs_path)
+		elif dotfiles:
+			backup_dotfiles(dotfiles_path)
+		elif installs:
+			backup_installs(installs_path)
+
+		return
 
 	# No CL options, input in terminal
-	if not complete and not dotfiles and not installs:
+	else:
 		selection = backup_prompt()
 
-	# CL options
-	else:
-		if complete:
-			selection = "all"
-		if dotfiles:
-			selection = "dotfiles"
-		if installs:
-			selection = "installs"
+		if selection == "all":
+			backup_all(dotfiles_path, installs_path)
+		elif selection == "dotfiles":
+			backup_dotfiles(dotfiles_path)
+		elif selection == "installs":
+			backup_installs(installs_path)
 
-	# Control Flow
-	if selection == "all":
-		backup_dotfiles()
-		backup_installs()
-
-	elif selection == "dotfiles":
-		backup_dotfiles()
-
-	elif selection == "installs":
-		backup_installs()
+		return
 
 
 if __name__ == '__main__':
