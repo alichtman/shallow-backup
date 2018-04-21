54ce54356ab0e0c9fa386a1caac05fb7b4e6516a
alichtman <aaronlichtman@gmail.com>
2018-04-21 03:50:36 -0500

    Updated constants for new pypi release

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 class Constants:
 	PROJECT_NAME='shallow-backup'
-	VERSION='0.4'
+	VERSION='0.5'
 	AUTHOR_GITHUB='alichtman'
 	AUTHOR_FULL_NAME='Aaron Lichtman'
-	DESCRIPTION="Easily create text documentation of installed applications, dotfiles, and more."
+	DESCRIPTION="Easily back up lightweight documentation of installed packages, dotfiles, and more."
 	URL='https://github.com/alichtman/shallow-backup',
 	AUTHOR_EMAIL='aaronlichtman@gmail.com',
