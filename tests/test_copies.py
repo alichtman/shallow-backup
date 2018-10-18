f4d0fe53418d0ea788be48d9021cb8b60955aa04
Peter Yasi <pyasi8192@gmail.com>
2018-10-18 19:09:47 -0400

    Fixes to broken tests file

--- tests/test_copies.py
+++ tests/test_copies.py
@@ -27,6 +27,7 @@ class TestCopyMethods:
 
     def teardown_method(self):
         shutil.rmtree(DIR_TO_BACKUP)
+        shutil.rmtree(BACKUP_DIR)
         os.remove(TEST_TEXT_FILE)
 
     def test_copy_file(self):
@@ -37,14 +38,14 @@ class TestCopyMethods:
         process =_copy_file(TEST_TEXT_FILE, BACKUP_DIR)
         assert process.returncode == 0
         assert os.path.isfile(TEST_TEXT_FILE)
-        assert os.path.isfile(os.path.join(BACKUP_DIR, TEST_TEXT_FILE)
+        assert os.path.isfile(os.path.join(BACKUP_DIR, TEST_TEXT_FILE))
 
     def test_copy_dir(self):
         """
         Test that copying a directory works as expected
         """
         # TODO: Test that all subfiles and folders are copied.
-        test_dir = 'test'
+        test_dir = 'test/'
         test_path = os.path.join(DIR_TO_BACKUP, test_dir)
         os.mkdir(test_path)
         process = _copy_dir(test_path, BACKUP_DIR)
