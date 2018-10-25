c0ee99124d8ff7fb4aba3d838f0cf90a55efd27f
redpfire <redpixafire@gmail.com>
2018-10-25 12:07:39 +0000

    colorama rewrite

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'inquirer>=2.2.0',
+        'colorama>=0.3.9',
         'Click'
     ],
 
--- stylize.py
+++ stylize.py
@@ -1,14 +1,13 @@
 
+from colorama import Fore, Style
 
-RED       = "\033[31m"
-GREEN     = "\033[92m"
-GRAY      = "\033[90m"
-CYAN      = "\033[36m"
-YELLOW    = "\033[33m"
-BLUE      = "\033[34m"
-RESET     = "\033[0m"
-UNDERLINE = "\033[4m"
-BRIGHT    = "\033[1m"
-NORMAL    = "\033[22m"
+RED       = Fore.RED
+GREEN     = Fore.GREEN
+CYAN      = Fore.CYAN
+YELLOW    = Fore.YELLOW
+BLUE      = Fore.BLUE
+RESET     = Style.RESET_ALL
+BRIGHT    = Style.BRIGHT
+NORMAL    = Style.NORMAL
 
 
