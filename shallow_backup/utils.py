cecf0558e99c7acce964a6ac4b71282385345fb2
Aaron Lichtman <aaronlichtman@gmail.com>
2018-10-29 23:41:29 +0000

    Update shallow_backup/utils.py
    Co-Authored-By: nunomdc <nunomdc@gmail.com>

--- shallow_backup/utils.py
+++ shallow_backup/utils.py
@@ -111,7 +111,7 @@ def home_prefix(path):
 	home_path = os.path.expanduser('~')
 	return os.path.join(home_path, path)
 
-def expand_path_to_absolute(path):
+def expand_to_abs_path(path):
 	"""
 	Expands relative and user's home paths to the respective absolute path.
 	:param path: Path to be expanded.
