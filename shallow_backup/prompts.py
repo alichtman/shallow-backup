21326f088db7e8182d293ae137538e603dfa1d02
Aaron Lichtman <aaronlichtman@gmail.com>
2018-10-30 03:44:57 -0500

    Convert add from CLI function to interface function

--- shallow_backup/prompts.py
+++ shallow_backup/prompts.py
@@ -16,7 +16,7 @@ def prompt_for_path_update(config):
 	print("{}{}Current shallow-backup path: {}{}{}".format(Fore.BLUE, Style.BRIGHT, Style.NORMAL, current_path, Style.RESET_ALL))
 
 	if prompt_yes_no("Would you like to update this?", Fore.GREEN):
-		print(Fore.GREEN + Style.BRIGHT + "Enter relative or absolute path:" + Style.RESET_ALL)
+		print_green_bold("Enter relative or absolute path:")
 		abs_path = expand_to_abs_path(input())
 		print(Fore.BLUE + "\nUpdating shallow-backup path to {}".format(abs_path) + Style.RESET_ALL)
 		config["backup_path"] = abs_path
@@ -38,24 +38,74 @@ def prompt_for_git_url(repo):
 
 def add_to_config_prompt():
 	"""
-	Show selector for dot or config. Then prompt for path, do error
-	checking and add it to the config.
+	Prompt sequence for a user to add a path to the config file under
+	either the dot or config sections.
 	"""
-	section = [inquirer.List('choice',
-	                         message=Fore.GREEN + Style.BRIGHT + "Which section would you like to add this to?" + Fore.BLUE,
-	                         choices=[' Dots',
-	                                  ' Configs',
-	                                  ])
+	add_prompt = [inquirer.List('choice',
+	                            message=Fore.GREEN + Style.BRIGHT + "Which section would you like to add this to?" + Fore.BLUE,
+	                            choices=[' Dots',
+	                                     ' Configs',
+	                                     ])
 	]
 
-	section = inquirer.prompt(section).get('choice').strip().lower()
-	valid_path = False
+	section = inquirer.prompt(add_prompt).get('choice').strip().lower()
+	config = get_config()
 
-	while not valid_path:
+	# Prompt until we get a valid path.
+	while True:
 		print_green_bold("Enter a path to add to {}:".format(section))
-		path = input()
+		input_path = expand_to_abs_path(input())
+		split_path = entered_path.split("/")
 
-	pass
+		# Check if path exists.
+		if not os.path.exists(entered_path):
+			print_red_bold("ERR: {} doesn't exist.".format(input_path))
+			continue
+
+		config_key = None
+		if section == "dots":
+			# Make sure it's actually a dotfile
+			if split_path[-1][0] != ".":
+				print_red_bold("ERR: Not a dotfile.")
+				continue
+
+			# Determine if adding to dotfiles or dotfolders
+			if not os.path.isdir(input_path):
+				config_key = "dotfiles"
+				print_blue_bold("Adding {} to dotfile backup.".format(input_path))
+			else:
+				config_key = "dotfolders"
+				print_blue_bold("Adding {} to dotfolder backup.".format(input_path))
+
+			# Add path to config ensuring no duplicates.
+			file_set = set(config[config_key])
+			file_set.update([path])
+			config[config_key] = list(file_set)
+			write_config(config)
+			break
+
+		elif section == "config":
+			# Prompt for folder name
+			print_green_bold("Enter a name for this config:".format(section))
+			dir_name = input()
+
+			# Handle plist and regular config.
+			if input_path.endswith(".plist"):
+				config_key = "plist_path_to_dest_map"
+				# Make dest path $SB/config/plist/FILENAME
+				to_add_to_cfg = (input_path, os.path.join("plist", dir_name))
+				print_blue_bold("Adding {} to plist backup.".format(input_path))
+			else:
+				config_key = "config_path_to_dest_map"
+				to_add_to_cfg = (input_path, dir_name)
+				print_blue_bold("Adding {} to config backup.".format(input_path))
+
+			# Get dictionary of {path_to_backup: dest, ...}
+			config_path_dict = config[config_key]
+			config_path_dict[to_add_to_cfg[0]] = to_add_to_cfg[1]
+			config[config_key] = config_path_dict
+			write_config(config)
+			break
 
 
 def actions_menu_prompt():
