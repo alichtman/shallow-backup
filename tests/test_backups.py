044f7c1df2cea986b1fccb950eca431730821e2c
Aaron Lichtman <aaronlichtman@gmail.com>
2018-11-14 07:25:35 -0600

    Added test for backing up dotfiles

--- tests/test_backups.py
+++ tests/test_backups.py
@@ -6,22 +6,23 @@ from shallow_backup.backup import backup_dotfiles
 from shallow_backup.config import safe_create_config, get_config_path
 
 BACKUP_DIR = 'shallow-backup-test-backups-dir'
+FAKE_HOME_DIR = 'shallow-backup-test-backups-src-dir'
 TEST_TEXT_CONTENT = 'THIS IS TEST CONTENT FOR THE DOTFILE'
-DIRS = [BACKUP_DIR]
+DIRS = [BACKUP_DIR, FAKE_HOME_DIR]
 DOTFILES = [
-	os.path.join(os.path.expanduser("~"), ".bashrc"),
-	os.path.join(os.path.expanduser("~"), ".bash_profile"),
-	os.path.join(os.path.expanduser("~"), ".gitconfig"),
-	os.path.join(os.path.expanduser("~"), ".profile"),
-	os.path.join(os.path.expanduser("~"), ".pypirc"),
-	os.path.join(os.path.expanduser("~"), ".shallow-backup"),
-	os.path.join(os.path.expanduser("~"), ".vimrc"),
-	os.path.join(os.path.expanduser("~"), ".zshrc")
+	os.path.join(FAKE_HOME_DIR, ".bashrc"),
+	os.path.join(FAKE_HOME_DIR, ".bash_profile"),
+	os.path.join(FAKE_HOME_DIR, ".gitconfig"),
+	os.path.join(FAKE_HOME_DIR, ".profile"),
+	os.path.join(FAKE_HOME_DIR, ".pypirc"),
+	os.path.join(FAKE_HOME_DIR, ".shallow-backup"),
+	os.path.join(FAKE_HOME_DIR, ".vimrc"),
+	os.path.join(FAKE_HOME_DIR, ".zshrc")
 ]
 
 DOTFOLDERS = [
-	os.path.join(os.path.expanduser("~"), ".ssh"),
-	os.path.join(os.path.expanduser("~"), ".vim")
+	os.path.join(FAKE_HOME_DIR, ".ssh"),
+	os.path.join(FAKE_HOME_DIR, ".vim")
 ]
 
 
@@ -33,46 +34,37 @@ class TestBackupMethods:
 	@staticmethod
 	def setup_method():
 		safe_create_config()
-
 		for directory in DIRS:
 			try:
 				os.mkdir(directory)
 			except FileExistsError as e:
-				print("NOTE: {} ALREADY EXISTS. REMOVING.".format(e.filename))
 				shutil.rmtree(directory)
 				os.mkdir(directory)
 
 		# Create all dotfiles and dotfolders
 		for file in DOTFILES:
-			if not os.path.exists(file):
-				with open(file, "w") as f:
-					f.write(TEST_TEXT_CONTENT)
+			with open(file, "w+") as f:
+				f.write(TEST_TEXT_CONTENT)
 
 		for folder in DOTFOLDERS:
-			if not os.path.exists(folder):
-				os.mkdir(folder)
-				for file in ["test1", "test2"]:
-					with open(os.path.join(folder, file), "w+") as f:
-						f.write(TEST_TEXT_CONTENT)
+			os.mkdir(folder)
+			for file in ["test1", "test2"]:
+				with open(os.path.join(folder, file), "w+") as f:
+					f.write(TEST_TEXT_CONTENT)
 
 	@staticmethod
 	def teardown_method():
-		print("Not deleting your dotfiles. You're welcome.")
-		# for file in DOTFILES:
-		# 	os.remove(file)
-		# for directory in DIRS:
-		# 	shutil.rmtree(directory)
-		# os.remove(get_config_path())
+		for folder in DIRS:
+			shutil.rmtree(folder)
 
 	def test_backup_dotfiles(self):
 		"""
-		Test that backing up a directory works as expected
+		Test backing up dotfiles and dotfolders.
 		"""
-		backup_dotfiles(os.path.join(BACKUP_DIR, "dotfiles"), skip=True)
-
 		dotfiles_path = os.path.join(BACKUP_DIR, "dotfiles")
+		backup_dotfiles(dotfiles_path, home_path=FAKE_HOME_DIR, skip=True)
 		assert os.path.isdir(dotfiles_path)
-
 		for path in DOTFILES:
+			print(path + " was backed up.")
 			backed_up_dot = os.path.join(dotfiles_path, os.path.split(path)[-1])
 			assert os.path.isfile(backed_up_dot)
