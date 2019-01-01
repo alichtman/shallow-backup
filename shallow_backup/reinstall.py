11434b694bd082a221c20ff26d3eeb908b97ebf3
Jason Phan <xormanatee@gmail.com>
2019-01-01 16:49:36 -0600

    Added check for existing but empty package directories

--- shallow_backup/reinstall.py
+++ shallow_backup/reinstall.py
@@ -61,7 +61,8 @@ def reinstall_packages_sb(packages_path):
 	"""
 	Reinstall all packages from the files in backup/installs.
 	"""
-	if not os.path.isdir(packages_path):
+	if not os.path.isdir(packages_path) or (os.path.isdir(packages_path) and 
+											not os.listdir(packages_path):
 		print_red_bold('No package backups found.')
 		sys.exit(1)
 
