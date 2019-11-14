d7d94524c6e2a9dd7dfe108d4f1be809a81c58d3
Aaron Lichtman <aaronlichtman@gmail.com>
2019-11-14 12:39:46 +0100

    Begin reworking tests
    Progress on #237

--- tests/test_backups.py
+++ tests/test_backups.py
@@ -43,6 +43,7 @@ class TestBackupMethods:
 
 		# Create all dotfiles and dotfolders
 		for file in DOTFILES:
+			print(f"Creating {file}")
 			with open(file, "w+") as f:
 				f.write(TEST_TEXT_CONTENT)
 
@@ -55,18 +56,20 @@ class TestBackupMethods:
 	@staticmethod
 	def teardown_method():
 		for folder in DIRS:
+			print(f"Removing {folder}")
 			shutil.rmtree(folder)
 
 	def test_backup_dotfiles(self):
 		"""
 		Test backing up dotfiles and dotfolders.
 		"""
-		dotfiles_path = os.path.join(BACKUP_DIR, "dotfiles")
-		backup_dotfiles(dotfiles_path, home_path=FAKE_HOME_DIR, skip=True)
-		assert os.path.isdir(dotfiles_path)
+		backup_dest_path = os.path.join(BACKUP_DIR, "dotfiles")
+		backup_dotfiles(backup_dest_path, home_path=FAKE_HOME_DIR, skip=True)
+		assert os.path.isdir(backup_dest_path)
 		for path in DOTFILES:
-			print("DOTFILES DIRECTORY CONTENTS:", os.listdir(dotfiles_path))
-			print(path + " being backed up.")
+			print(f"BACKUP DESTINATION DIRECTORY ({backup_dest_path}) CONTENTS:", os.listdir(backup_dest_path))
+			print(path + " should already be backed up.")
 			print("CWD:", os.getcwd())
-			backed_up_dot = os.path.join(dotfiles_path, os.path.split(path)[-1])
+			backed_up_dot = os.path.join(backup_dest_path, os.path.split(path)[-1])
+			print(f"Backed up dot: {backed_up_dot}")
 			assert os.path.isfile(backed_up_dot)
