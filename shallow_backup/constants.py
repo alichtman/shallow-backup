b75e03ffdf41abf9f6c488bc9842abb8132566d2
Aaron Lichtman <aaronlichtman@gmail.com>
2020-03-24 23:05:01 -0500

    Version bump to v4.0.1

--- shallow_backup/constants.py
+++ shallow_backup/constants.py
@@ -1,6 +1,6 @@
 class ProjInfo:
 	PROJECT_NAME = 'shallow-backup'
-	VERSION = '4.0'
+	VERSION = '4.0.1'
 	AUTHOR_GITHUB = 'alichtman'
 	AUTHOR_FULL_NAME = 'Aaron Lichtman'
 	DESCRIPTION = "Easily create lightweight backups of installed packages, dotfiles, and more."
